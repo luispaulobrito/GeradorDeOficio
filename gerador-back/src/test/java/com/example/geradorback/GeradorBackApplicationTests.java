f5077bad221dcb08686cd7772f29510a21663594
Luis Paulo <luiss_paullo@hotmail.com>
2023-12-12 23:06:35 -0300

    front e back base

---
+++ gerador-back/src/test/java/com/example/geradorback/GeradorBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geradorback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
