c635c1106ffba80b95eed1a7187370e596dd92dd
Luis Paulo <luiss_paullo@hotmail.com>
2024-01-14 20:50:30 -0300

    layout tela login

--- gerador-front/src/app/auth/auth.module.ts
+++ gerador-front/src/app/auth/auth.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -11,7 +16,12 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class AuthModule { }
