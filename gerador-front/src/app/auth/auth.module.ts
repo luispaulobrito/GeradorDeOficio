a2a6d9b01ba2d9b8dd6f9f93fa58d5661accba93
Luis Paulo <luiss_paullo@hotmail.com>
2024-01-16 14:30:34 -0300

    metodo para login criado

--- gerador-front/src/app/auth/auth.module.ts
+++ gerador-front/src/app/auth/auth.module.ts
@@ -8,6 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -17,6 +18,7 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [
     CommonModule,
     AuthRoutingModule,
+    ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
---
+++ gerador-front/src/app/core/api/base.api.ts
@@ -0,0 +1,41 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/app/environment/environment';
+import { StorageService } from '../services/storage.service';
+
+export class BaseAPI {
+  protected apiUrl: string = environment.apiUrl;
+
+  constructor(
+    protected httpClient: HttpClient,
+    protected storageService: StorageService
+  ) {}
+
+  /**
+   * Configura e retorna um conjunto de cabeçalhos HTTP padrão para serem utilizados em requisições HTTP.
+   * @returns Um objeto contendo os cabeçalhos HTTP configurados.
+   */
+  public setHeaders(): HttpHeaders['headers'] {
+    const bearerToken = this.storageService.getItem('token');
+
+    const headers: HttpHeaders['headers'] = {
+      language: 'pt-br',
+      ...(bearerToken && { authorization: `Bearer ${bearerToken}` }),
+      'Content-Type': 'application/json',
+    };
+
+    return headers;
+  }
+
+  /**
+   * Realiza uma requisição HTTP POST para a URL da API.
+   *
+   * @param data Os dados a serem enviados na requisição.
+   * @returns Um Observable com a resposta da API se a requisição for bem-sucedida, ou uma mensagem de erro em caso de falha.
+   */
+  public post<TD, TR>(data: TD): Observable<TR> {
+    const headers = this.setHeaders();
+
+    return this.httpClient.post<TR>(this.apiUrl, data, { headers });
+  }
+}
