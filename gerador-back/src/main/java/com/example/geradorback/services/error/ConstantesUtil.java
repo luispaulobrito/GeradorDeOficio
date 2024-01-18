3096284d65f00f45834177d27bea6bfe25de7d30
Luis Paulo <luiss_paullo@hotmail.com>
2024-01-18 10:38:19 -0300

    excecoes adicionadas

---
+++ gerador-back/src/main/java/com/example/geradorback/services/error/ConstantesUtil.java
@@ -0,0 +1,9 @@
+package com.example.geradorback.services.error;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class ConstantesUtil {
+    public static final String ERROR_TITLE = "Ocorreu um erro";
+    public static final String USUARIO_EXISTE = "Usuário já existe";
+}
