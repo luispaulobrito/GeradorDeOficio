c263cbeb25bb331b62313c585e40f737af7a356f
Luis Paulo <luiss_paullo@hotmail.com>
2024-01-18 10:38:39 -0300

    mapper adicionado

---
+++ gerador-back/src/main/java/com/example/geradorback/services/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.example.geradorback.services.mapper;
+
+import com.example.geradorback.domain.User;
+import com.example.geradorback.services.records.RegisterDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper extends EntityMapper<RegisterDTO, User> {
+}
