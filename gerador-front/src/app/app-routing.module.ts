fd3098272cff8a3d3669df83f3c5a1efcd9fe784
Luis Paulo <luiss_paullo@hotmail.com>
2024-01-19 21:17:26 -0300

    direcionamento para tela home apos login configurado

--- gerador-front/src/app/app-routing.module.ts
+++ gerador-front/src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule, Type } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthModule } from './auth/auth.module';
+import { PagesModule } from './pages/pages.module';
+import { AuthGuardService } from './core/services/auth-guard.service';
 
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: (): Promise<Type<AuthModule> | AuthModule[]> =>
       import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'pages',
+    loadChildren: (): Promise<Type<PagesModule> | PagesModule[]> =>
+      import('./pages/pages.module').then((m) => m.PagesModule),
+    canActivate: [AuthGuardService],
   },
   { path: '**', redirectTo: 'auth' },
 ];
