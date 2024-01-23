f2e159bb42068126d91277b7129f9f211e5821bd
Luis Paulo <luiss_paullo@hotmail.com>
2024-01-22 22:57:47 -0300

    configuracao de sppiner e redefinicao de cores

--- gerador-front/src/app/app.module.ts
+++ gerador-front/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SpinnerComponent } from './theme/components/spinner/spinner/spinner.component';
+import { SpinnerComponent } from './theme/components/spinner/spinner.component';
 import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
--- gerador-front/src/app/auth/login/login.component.ts
+++ gerador-front/src/app/auth/login/login.component.ts
@@ -6,6 +6,7 @@ import { EnumStorageType } from 'src/app/core/common/enums/enum.storage.type';
 import { IResponseLogin } from 'src/app/core/models/IResponseLogin';
 import { Router } from '@angular/router';
 import { AuthAPI } from 'src/app/core/api/auth.api';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -36,26 +37,36 @@ export class LoginComponent {
     });
   }
 
+  /**
+   * Realiza o processo de login.
+   * - Define o token de acordo com a opção de "Lembrar de mim".
+   * - Navega para 'pages' em caso de sucesso.
+   * - Exibe uma mensagem de erro em caso de falha.
+   */
   public login(): void {
     this.loaderService.setLoading(true);
 
-    this.authAPI.login(this.loginForm.value).subscribe(
-      (response: IResponseLogin) => {
-        const storageType = this.loginForm.get('isRememberEnabled')?.value
-          ? EnumStorageType.LOCAL
-          : EnumStorageType.SESSION;
-        this.storageService.setItem('token', response.token, storageType);
-        this.router.navigate(['/pages']);
-      },
-      (error) => {
-        this.errorMessage =
-          error.error && error.error.message
-            ? error.error.message
-            : 'Ocorreu um erro na comunicação com o servidor. Tente novamente.';
-      },
-      () => {
-        this.loaderService.setLoading(false);
-      }
-    );
+    this.authAPI
+      .login(this.loginForm.value)
+      .pipe(
+        finalize(() => {
+          this.loaderService.setLoading(false);
+        })
+      )
+      .subscribe(
+        (response: IResponseLogin) => {
+          const storageType = this.loginForm.get('isRememberEnabled')?.value
+            ? EnumStorageType.LOCAL
+            : EnumStorageType.SESSION;
+          this.storageService.setItem('token', response.token, storageType);
+          this.router.navigate(['/pages']);
+        },
+        (error) => {
+          this.errorMessage =
+            error.error && error.error.message
+              ? error.error.message
+              : 'Ocorreu um erro na comunicação com o servidor. Tente novamente.';
+        }
+      );
   }
 }
