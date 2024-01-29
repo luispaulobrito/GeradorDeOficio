051605836df9f22b8d6475cd25b7ec7cc6c76bff
Luis Paulo <luiss_paullo@hotmail.com>
2024-01-29 19:23:14 -0300

    metodos para solicitar a mudança de password

--- gerador-front/src/app/auth/password-forgot/password-forgot.component.ts
+++ gerador-front/src/app/auth/password-forgot/password-forgot.component.ts
@@ -1,27 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { finalize } from 'rxjs';
+import { ForgotPasswordAPI } from 'src/app/core/api/forgot-password.api';
 import { EmailService } from 'src/app/core/services/email.service';
 import { LoaderService } from 'src/app/core/services/loader.service';
+import { ModalService } from 'src/app/core/services/modal.service';
 
 @Component({
   selector: 'app-password-forgot',
   templateUrl: './password-forgot.component.html',
   styleUrls: ['./password-forgot.component.scss'],
 })
-export class PasswordForgotComponent implements OnInit{
+export class PasswordForgotComponent implements OnInit {
   public emailForm: FormGroup;
   public login!: string;
 
   constructor(
-    // private forgotPasswordAPI: ForgotPasswordAPI,
-    private router: Router,
+    private forgotPasswordAPI: ForgotPasswordAPI,
     private loaderService: LoaderService,
-    // private modalService: ModalService,
+    private modalService: ModalService,
     private emailService: EmailService
   ) {
     this.emailForm = new FormGroup({
-      login: new FormControl('', [Validators.required, Validators.pattern(/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/)]),
+      login: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/),
+      ]),
     });
   }
 
@@ -39,5 +43,39 @@ export class PasswordForgotComponent implements OnInit{
     }
   }
 
-  onSubmit(){}
+  /**
+   * Manipula o envio do formulário de redefinição de senha.
+   * Este método chama a API de redefinição de senha e exibe um diálogo de sucesso ou captura erros.
+   */
+  public onSubmit(): void {
+    if (this.emailForm.invalid) {
+      return;
+    }
+
+    this.loaderService.setLoading(true);
+    
+    this.forgotPasswordAPI
+      .passwordReset(this.emailForm.value.login)
+      .pipe(
+        finalize(() => {
+          this.loaderService.setLoading(false);
+        })
+      )
+      .subscribe(
+        () => {
+          this.modalService.showDialog({
+            title: 'Sucesso',
+            message: 'Seu e-mail cadastrado receberá em breve instruções para redefinir a senha.',
+            feedback: 'success',
+          });
+        },
+        () => {
+          this.modalService.showDialog({
+            title: 'Falha!',
+            message: 'Ocorreu um erro na comunicação com o servidor. Tente novamente.',
+            feedback: 'error',
+          });
+        }
+      );
+  }
 }
