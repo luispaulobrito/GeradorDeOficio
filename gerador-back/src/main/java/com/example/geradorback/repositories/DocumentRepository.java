e2bd1117d320bde47f5541d57346a3d931f4eb33
Luis Paulo <luiss_paullo@hotmail.com>
2024-03-04 21:47:22 -0300

    metodo repository encontrar documento por ano e tipo

--- gerador-back/src/main/java/com/example/geradorback/repositories/DocumentRepository.java
+++ gerador-back/src/main/java/com/example/geradorback/repositories/DocumentRepository.java
@@ -1,9 +1,11 @@
 package com.example.geradorback.repositories;
 
 import com.example.geradorback.domain.Document;
+import com.example.geradorback.services.enums.DocumentTypeEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DocumentRepository extends JpaRepository<Document, String> {
+    Document findFirstByDocumentYearAndDocumentTypeEnumOrderByDocumentNumberDesc(Integer year, DocumentTypeEnum type);
 }
