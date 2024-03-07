458550be023796194499638527c649bb9e597cc3
Luis Paulo <luiss_paullo@hotmail.com>
2024-03-07 15:09:04 -0300

    ajuste imports

--- gerador-back/src/main/java/com/example/geradorback/domain/PasswordResetToken.java
+++ gerador-back/src/main/java/com/example/geradorback/domain/PasswordResetToken.java
@@ -1,7 +1,17 @@
 package com.example.geradorback.domain;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
