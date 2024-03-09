1d36e2bb302bf90a79dea6dd724a7d924222d5c3
Luis Paulo <luiss_paullo@hotmail.com>
2024-03-08 21:48:33 -0300

    ajustes nos services

--- gerador-back/src/main/java/com/example/geradorback/services/PasswordResetService.java
+++ gerador-back/src/main/java/com/example/geradorback/services/PasswordResetService.java
@@ -3,7 +3,9 @@ package com.example.geradorback.services;
 import com.example.geradorback.domain.PasswordResetToken;
 import com.example.geradorback.domain.User;
 import com.example.geradorback.repositories.PasswordResetTokenRepository;
-import com.example.geradorback.repositories.UserRepository;
+import com.example.geradorback.services.mapper.UserMapper;
+import com.example.geradorback.services.records.RegisterDTO;
+import com.example.geradorback.services.records.RequestPasswordDTO;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +17,7 @@ import java.util.Objects;
 public class PasswordResetService {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @Autowired
     private PasswordResetTokenRepository tokenRepository;
@@ -23,18 +25,22 @@ public class PasswordResetService {
     @Autowired
     private EmailService emailService;
 
+    @Autowired
+    private UserMapper userMapper;
+
     private static final Logger logger = LogManager.getLogger(PasswordResetService.class);
 
-    public boolean initiatePasswordReset(String login) {
-        logger.info("Buscando usuário: {}", login);
-        User user = userRepository.findByLogin(login);
-        if (Objects.isNull(user)) {
+    public boolean initiatePasswordReset(RequestPasswordDTO requestPasswordDTO) {
+        logger.info("Buscando usuário: {}", requestPasswordDTO.login());
+        RegisterDTO registerDTO = userService.findByLogin(requestPasswordDTO);
+        if (Objects.isNull(registerDTO)) {
             return false;
         }
+        User user = userMapper.toEntity(registerDTO);
         PasswordResetToken token = new PasswordResetToken();
         emailService.sendPasswordResetEmail(user.getId(), user.getLogin(), token.getToken());
         tokenRepository.save(token);
-        logger.info("E-mail de redefinição de senha iniciado para o usuário: {}", login);
+        logger.info("E-mail de redefinição de senha iniciado para o usuário: {}", requestPasswordDTO.login());
         return true;
     }
 }
